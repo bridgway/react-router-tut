1bcd8af92b864f7b353c289a68fc69b40161c898
Ben Ridgway <benridgway83@gmail.com>
2018-02-16 19:24:58 +0000

    players page part 2

--- src/components/Players.js
+++ src/components/Players.js
@@ -6,45 +6,74 @@ import { parse } from 'query-string'
 import slug from 'slug'
 
 export default class Players extends Component {
+  state = {
+    players: [],
+    loading: true,
+  }
+  componentDidMount () {
+    const { location } = this.props
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers()
+  }
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then((players) => this.setState(() => ({
+        loading: false,
+        players,
+      })))
+  }
+  render () {
+    const { players, loading } = this.state
+    const { match, location } = this.props
 
-	state = {
-		players: [],
-		loading: true,
-	}
+    return (
+      <div className='container two-column'>
+        <Sidebar
+          loading={loading}
+          title='Players'
+          list={players.map((player) => player.name)}
+          {...this.props}
+        />
 
-	componentDidMount () {
-		const { location } = this.props
-		location.search
-			? this.fetchPlayers(parse(location.search).teamId)
-			: this.fetchPlayers()
-	}
+        {loading === false && location.pathname === '/players'
+          ? <div className='sidebar-instruction'>Select a Player</div>
+          : null}
 
-	fetchPlayers = (teamId) => {
-		getPlayers(teamId)
-			.then((players) => this.setState(() => ({
-				loading: false,
-				players,
-			})))
-	}
+        <Route path={`${match.url}/:playerId`} render={({match}) => {
+          if (loading === true) return null
 
-	render() {
+          const {
+            name, position, teamId, number, avatar, apg, ppg, rpg, spg
+          } = players.find((player) => slug(player.name) === match.params.playerId)
 
-		const { players, loading } = this.state
-		const { match, location } = this.props
-
-		return (
-			<div className='container two-column'>
-				<Sidebar
-					loading={loading}
-					title='Players'
-					list={players.map((player) => player.name)}
-					{...this.props}
-				/>
-
-				{loading === false && location.pathname === '/players'
-					? <div className='sidebar-instruction'>Select a Player</div>
-					: null}
-			</div>
-		)
-	}
+          return (
+            <div className='panel'>
+              <img className='avatar' src={`${avatar}`} alt={`${name}'s avatar`} />
+              <h1 className='medium-header'>{name}</h1>
+              <h3 className='header'>#{number}</h3>
+              <div className='row'>
+                <ul className='info-list' style={{marginRight: 80}}>
+                  <li>Team
+                    <div>
+                      <Link style={{color: '#68809a'}} to={`/${teamId}`}>
+                        {teamId[0].toUpperCase() + teamId.slice(1)}
+                      </Link>
+                    </div>
+                  </li>
+                  <li>Position<div>{position}</div></li>
+                  <li>PPG<div>{ppg}</div></li>
+                </ul>
+                <ul className='info-list'>
+                  <li>APG<div>{apg}</div></li>
+                  <li>SPG<div>{spg}</div></li>
+                  <li>RPG<div>{rpg}</div></li>
+                </ul>
+              </div>
+            </div>
+          )
+        }} />
+      </div>
+    )
+  }
 }
